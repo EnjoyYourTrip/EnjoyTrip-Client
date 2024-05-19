105f47a16e594f6e4ad201aefb819b0dd0a94e19
inhohyun <inhohyun36@gmail.com>
2024-05-19 13:20:45 +0900

    feat: 라우터 네비게이션 가드 추가

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,6 +11,30 @@ import QnADetail from '../views/QnA/QnADetailView.vue';
 import QnAEdit from '../views/QnA/QnAEditView.vue';
 import PlanDetailView from '../views/plan/PlanDetailView.vue';
 import UserMypage from '@/views/member/MypageView.vue';
+import { useMemberStore } from '@/stores/memberStore';
+const onlyAuthUser = async (to, from, next) => {
+  const memberStore = useMemberStore();
+  let token = sessionStorage.getItem('accessToken');
+  console.log(
+    '로그인 처리 전================================',
+    memberStore.userInfo,
+    token,
+  );
+
+  if (memberStore.userInfo != null && token) {
+    console.log('토큰 유효성 체크');
+    await memberStore.getUserInfo(token);
+  }
+  if (!memberStore.isValidToken || memberStore.userInfo === null) {
+    alert('로그인이 필요한 페이지입니다.');
+    // next({ name: "login" });
+    router.push({ name: 'login' }); // 로그인되지 않은 사용자라면 로그인창으로 이동시키기
+  } else {
+    console.log('로그인 했다!');
+    next();
+  }
+};
+
 const routes = [
   {
     path: '/',
@@ -20,7 +44,8 @@ const routes = [
   {
     path: '/mypage',
     name: 'user-mypage',
-    component: UserMypage, // 마이페이지 컴포넌트 연결
+    beforeEnter: onlyAuthUser, //마이페이지 전에 로그인 여부파악
+    component: UserMypage,
   },
   {
     path: '/regist',
@@ -35,6 +60,7 @@ const routes = [
   {
     path: '/createPlan',
     name: 'createPlan',
+    beforeEnter: onlyAuthUser, // 여행계획 생성 전에 로그인 확인
     component: CreatePlan,
   },
   {
@@ -55,6 +81,7 @@ const routes = [
   {
     path: '/QnACreate',
     name: 'QnACreate',
+    beforeEnter: onlyAuthUser, // qna 생성 전에 로그인 여부 파악하기
     component: QnACreate,
   },
   {
@@ -65,6 +92,7 @@ const routes = [
   {
     path: '/QnAEdit/:id',
     name: 'QnAEdit',
+    beforeEnter: onlyAuthUser, // qna 수정 전에 로그인여부 파악하기
     component: QnAEdit,
   },
 ];
